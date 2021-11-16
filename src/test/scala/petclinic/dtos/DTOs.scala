d9e9baccfdd7ea9b6bbdcc7e5d8f3ade02330875
Aleksandar Karamiflov <aleksandar.karamfilov.extern@joyn.de>
2021-11-17 00:12:32 +0200

    adjustments

--- src/test/scala/petclinic/dtos/DTOs.scala
+++ src/test/scala/petclinic/dtos/DTOs.scala
@@ -1,4 +1,14 @@
 package petclinic.dtos
 
-case class Speciality(id: Int, name: String)
+import com.google.gson.Gson
+
+case class Speciality(id: Int = 1, name: String = "surgery")
 case class Vet(firstName: String, lastName: String, specialities: Array[Speciality])
+
+
+object Test extends App {
+  val Gson = new Gson().newBuilder().setPrettyPrinting().create()
+  println(Gson.toJson(Vet("Alex", "Karamfilov", null)))
+  println(Gson.toJson(Vet("Ivan", "Ivanov", Array(Speciality(2, "denistry")))))
+  println("""{ "name": "Alex"}""")
+}
--- src/test/scala/petclinic/simulations/PetClinicSimulation.scala
+++ src/test/scala/petclinic/simulations/PetClinicSimulation.scala
@@ -10,28 +10,47 @@ import petclinic.dtos.Vet
 import scala.concurrent.duration.DurationInt
 
 
-
 class PetClinicSimulation extends Simulation {
     private val PetClinicBaseUrl = "http://localhost:9966/petclinic/api"
     private val headers = Map("Content-Type" -> "application/json", "Authorization" -> "Basic YWRtaW46YWRtaW4=")
-    private val httpProtocols = http.baseUrl(PetClinicBaseUrl).headers(headers)
     private val GeneralFeeder = Iterator.continually(Map("name" -> "test"))
     private val VetFeeder = csv("data/vets.csv").random
     private val virtualUsers: String = sys.props.getOrElse("users", "1")
     private val rampUpTime: String = sys.props.getOrElse("rampUp", "10")
     private val thinkingTime: String = sys.props.getOrElse("thinkTime", "1")
-    private val Gson: Gson = new Gson().newBuilder().setPrettyPrinting().create();
+    private val Gson: Gson = new Gson().newBuilder().setPrettyPrinting().create()
+    private val httpConf = http.baseUrl(PetClinicBaseUrl)
 
 
     def createVet(vet: Vet) = {
       exec(http("Create new vet")
-        .post(s"$PetClinicBaseUrl/vets")
-        .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+        .post(s"${PetClinicBaseUrl}/vets")
+        .headers(headers)
         .body(StringBody(Gson.toJson(vet)))
         .check(status.is(201)))
     }
 
+    def updateVet(id: Int, vet: Vet) = {
+        exec(http("Update existing vet")
+          .post(s"${PetClinicBaseUrl}/vets/$id")
+          .headers(headers)
+          .body(StringBody(Gson.toJson(vet)))
+          .check(status.is(201)))
+    }
 
+    def getAllVets = {
+        exec(http("Get all vets")
+          .get(s"${PetClinicBaseUrl}/vets")
+          .headers(headers)
+          .check(status.is(200)))
+    }
+
+    def getSingleVet(id: String) = {
+        exec(http("Get all vets")
+          .get(s"${PetClinicBaseUrl}/vets/$id")
+          .headers(headers)
+          .check(status.is(200)))
+    }
 
 
     val createVetScenario = scenario("Create new vet")
@@ -39,5 +58,5 @@ class PetClinicSimulation extends Simulation {
       .exec(createVet(Vet("${firstName}", "${lastName}", null))).pause(thinkingTime)
 
     setUp(createVetScenario.inject(rampUsers(virtualUsers.toInt).during(rampUpTime.toInt.seconds)))
-      .protocols(httpProtocols)
+      .protocols(httpConf)
 }
